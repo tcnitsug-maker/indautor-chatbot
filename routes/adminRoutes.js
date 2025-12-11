2b9197d6e5f418fdfd828917a6849d09f4747ef4
tcnitsug-maker <tcnitsug@gmail.com>
2025-12-10 22:00:40 -0600

    Update adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -4,12 +4,16 @@ const { getAllMessages, deleteMessage } = require("../controllers/adminControlle
 
 const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || "12345";
 
+// Login de admin
 router.post("/login", (req, res) => {
   const { password } = req.body;
   res.json({ ok: password === ADMIN_PASSWORD });
 });
 
+// Historial de mensajes
 router.get("/messages", getAllMessages);
+
+// Eliminar mensaje
 router.delete("/messages/:id", deleteMessage);
 
 module.exports = router;
