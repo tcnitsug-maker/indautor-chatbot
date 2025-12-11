6ee87d7c48030411da791fec96ad165c7b374a33
tcnitsug-maker <tcnitsug@gmail.com>
2025-12-10 23:50:11 -0600

    Update CustomReply.js

--- models/CustomReply.js
+++ models/CustomReply.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 const CustomReplySchema = new mongoose.Schema(
   {
-    question: { type: String, required: true },   // Pregunta base
-    answer:   { type: String, required: true },   // Respuesta del bot
+    question: { type: String, required: true },   // Texto base de la pregunta
+    answer: { type: String, required: true },     // Respuesta que dará el bot
     keywords: [{ type: String }],                 // Palabras clave opcionales
-    enabled:  { type: Boolean, default: true },   // Activada o no
+    enabled: { type: Boolean, default: true },    // Activada o no
   },
   { timestamps: true }
 );
