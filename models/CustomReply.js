30000a8ce40b7e5fb5b616864d3a40f74ebe07ca
tcnitsug-maker <tcnitsug@gmail.com>
2025-12-14 11:30:01 -0600

    Update CustomReply.js

--- models/CustomReply.js
+++ models/CustomReply.js
@@ -1,10 +1,26 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
-const CustomReplySchema = new mongoose.Schema({
-  question: String,
-  answer: String,
-  keywords: [String],
-  enabled: { type: Boolean, default: true },
-});
+const CustomReplySchema = new mongoose.Schema(
+  {
+    trigger: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+    response: {
+      type: String,
+      required: true
+    },
+    active: {
+      type: Boolean,
+      default: true
+    }
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model("CustomReply", CustomReplySchema);
+const CustomReply = mongoose.model("CustomReply", CustomReplySchema);
+
+export default CustomReply;
+
