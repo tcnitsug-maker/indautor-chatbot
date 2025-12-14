b53c6a1548ec5c68c1926a7a1a7012505cc5cf8d
tcnitsug-maker <tcnitsug@gmail.com>
2025-12-13 18:13:47 -0600

    Update AdminUser.js

--- models/AdminUser.js
+++ models/AdminUser.js
@@ -1,1 +1,29 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const AdminUserSchema = new mongoose.Schema(
+  {
+    username: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    role: {
+      type: String,
+      enum: ["superadmin", "admin", "viewer"],
+      default: "viewer",
+    },
+    active: { type: Boolean, default: true },
+  },
+  { timestamps: true }
+);
+
+AdminUserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+AdminUserSchema.methods.comparePassword = function (plain) {
+  return bcrypt.compare(plain, this.password);
+};
+
+module.exports = mongoose.model("AdminUser", AdminUserSchema);
 
