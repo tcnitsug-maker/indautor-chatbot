f7db440f51b2b00ae8de08dd5413ce65abaa282f
tcnitsug-maker <tcnitsug@gmail.com>
2025-12-13 23:40:39 -0600

    Update AdminUser.js

--- models/AdminUser.js
+++ models/AdminUser.js
@@ -15,10 +15,9 @@ const AdminUserSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-AdminUserSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+AdminUserSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
   this.password = await bcrypt.hash(this.password, 10);
-  next();
 });
 
 AdminUserSchema.methods.comparePassword = function (plain) {
@@ -26,4 +25,3 @@ AdminUserSchema.methods.comparePassword = function (plain) {
 };
 
 module.exports = mongoose.model("AdminUser", AdminUserSchema);
-
