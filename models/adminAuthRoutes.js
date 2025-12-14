a5a62ea83e8c8bee3cef17bb2dd6f6287f52bf20
tcnitsug-maker <tcnitsug@gmail.com>
2025-12-13 18:14:40 -0600

    Create adminAuthRoutes.js

---
+++ models/adminAuthRoutes.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+const jwt = require("jsonwebtoken");
+const AdminUser = require("../models/AdminUser");
+
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await AdminUser.findOne({ username, active: true });
+  if (!user) {
+    return res.status(401).json({ error: "Credenciales inválidas" });
+  }
+
+  const ok = await user.comparePassword(password);
+  if (!ok) {
+    return res.status(401).json({ error: "Credenciales inválidas" });
+  }
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+      username: user.username,
+      role: user.role,
+    },
+    process.env.ADMIN_JWT_SECRET,
+    { expiresIn: "8h" }
+  );
+
+  res.json({
+    token,
+    role: user.role,
+    username: user.username,
+  });
+});
+
+module.exports = router;
