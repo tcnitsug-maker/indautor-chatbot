1b4d29486594be8e543a33d0394474834d240692
tcnitsug-maker <tcnitsug@gmail.com>
2025-12-18 13:00:28 -0600

    Delete middlewares directory

--- middlewares/requireRole.js
+++
@@ -1,9 +0,0 @@
-
-export function requireRole(roles = []) {
-  return (req, res, next) => {
-    if (!req.admin || !roles.includes(req.admin.role)) {
-      return res.status(403).json({ error: "Acceso denegado" });
-    }
-    next();
-  };
-}
