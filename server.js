98c6b732adf42cdae9b84ccafae8dc7aba2b54b1
tcnitsug-maker <tcnitsug@gmail.com>
2025-12-18 13:48:08 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-app.use(express.static(path.join(__dirname, "public")));
+app.use((req, res, next) => {
+  if (req.url.endsWith(".js")) {
+    res.setHeader("Content-Type", "application/javascript; charset=UTF-8");
+  }
+  if (req.url.endsWith(".css")) {
+    res.setHeader("Content-Type", "text/css; charset=UTF-8");
+  }
+  next();
+});
+
